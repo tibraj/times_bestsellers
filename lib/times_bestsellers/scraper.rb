81bdf0fd9753e0af5068bbe0eca6eafc5492afbb
Taulant Ibraj <tibraj94@gmail.com>
2020-05-04 15:37:47 +0000

    Scraper class

--- lib/times_bestsellers/scraper.rb
+++ lib/times_bestsellers/scraper.rb
@@ -1,3 +1,17 @@
 class TimesBestsellers::Scraper
   
+  def self.scrape_novels 
+    doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/trade-fiction-paperback/"))
+    novels = doc.css("div.css-46u0bd section.css-19a8fbx li.css-13y32ub")
+    novels.each do |novel|
+      Novel.new(
+        title = novel.css("h3.css-5pe77f").text.strip,
+        author = novel.css("p.css-hjukut").text.strip,
+        publisher = novel.css("p.css-heg334").text.strip,
+        weeks_on_list = novel.css("p.css-1o26r9v").text.strip,
+        synopsis = novel.css("p.css-14lubdp").text.strip
+        )
+    end 
+  end 
+    
 end
