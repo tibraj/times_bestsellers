70974e5b2a75a100ded982aaa7f4355796abd47c
Taulant Ibraj <tibraj94@gmail.com>
2020-05-04 15:36:52 +0000

    Novel class

--- lib/times_bestsellers/novel.rb
+++ lib/times_bestsellers/novel.rb
@@ -1,3 +1,27 @@
 class TimesBestsellers::Novel 
   
+  attr_accessor :title, :author, :publisher, :weeks_on_list, :synopsis
+  
+  @@all = []
+  
+  def initialize(title, author, publisher, weeks_on_list, synopsis)
+    @title = title 
+    @author = author 
+    @publisher = publisher 
+    @weeks_on_list = weeks_on_list
+    @synopsis = synopsis
+    save
+  end 
+  
+  def save
+    @@all << self unless @@all.include?(self)
+  end 
+  
+  def self.all 
+    if @@all.empty?
+      Scraper.scrape_novels
+    end
+    @@all 
+  end
+  
 end
