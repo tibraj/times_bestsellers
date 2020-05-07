95c867c4d76e98d15ae7c14cbe626c5560f40c7a
Taulant Ibraj <tibraj94@gmail.com>
2020-05-07 14:45:32 +0000

    some restructuring

--- lib/times_bestsellers/cli.rb
+++ lib/times_bestsellers/cli.rb
@@ -31,7 +31,6 @@ class TimesBestsellers::CLI
       puts @novels[user_choice.to_i - 1].weeks_on_list
       puts "Synopsis: " + "#{@novels[user_choice.to_i - 1].synopsis}"
     else
-      puts "-----"
       puts "*Please enter a number between 1 & 15*"
       puts "-----"
       show_info 
