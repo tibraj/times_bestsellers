26bb77d9a37669bc61e50ba445d09996b8a6477a
Taulant Ibraj <tibraj94@gmail.com>
2020-05-10 21:33:11 +0000

    done

--- lib/times_bestsellers/cli.rb
+++ lib/times_bestsellers/cli.rb
@@ -3,13 +3,12 @@ class TimesBestsellers::CLI
   def call 
     puts "Welcome! Here is the New York Times' Bestsellers List for Paperback Fiction:"
     puts "-----"
-      novels
-      show_list 
-      puts "-----"
-      novel_selection
-      puts "-----"
-      continue? 
-    end
+    novels
+    show_list 
+    puts "-----"
+    show_info
+    puts "-----"
+    continue? 
   end
 
   def novels
@@ -22,31 +21,21 @@ class TimesBestsellers::CLI
       "done"
     end 
   end 
-  
-  def valid? (input, array)
-    input.to_i <= array.length && input.to_i > 0
-  end 
-  
-  def novel_selection
+
+  def show_info
     puts "Which novel would you like to learn more about? (Enter 1-15)"
+    user_choice = gets.strip
     puts "-----"
-    novel_selected = gets.strip.to_i 
-    if valid?(novel_selected, @novels)
-      show_info(novel_selected)
+    if user_choice.to_i <= 15 && user_choice.to_i > 0
+      puts "Publisher: " + "#{@novels[user_choice.to_i - 1].publisher}"
+      puts @novels[user_choice.to_i - 1].weeks_on_list
+      puts "Synopsis: " + "#{@novels[user_choice.to_i - 1].synopsis}"
     else
-      puts "-----"
       puts "*Please enter a number between 1 & 15*"
       puts "-----"
-      novel_selection
+      show_info 
     end 
   end 
-    
-  def show_info(novel_selected)
-    puts "-----"
-      puts "Publisher: " + "#{novels[novel_selected.to_i - 1].publisher}"
-      puts novels[novel_selected.to_i - 1].weeks_on_list
-      puts "Synopsis: " + "#{novels[novel_selected.to_i - 1].synopsis}"
-  end  
 
   def continue?
     puts "Would you like another look at the list? (Y/N)"
@@ -54,10 +43,15 @@ class TimesBestsellers::CLI
     if user_action.upcase == "Y"
       call 
     elsif user_action.upcase == "N"
-      puts "Goodbye!"
+      puts "Goodbye"
     else
       puts "-----"
       puts "*Please enter Y or N*"
+      puts "-----"
       continue?
     end
   end
+
+end 
+
+#done
